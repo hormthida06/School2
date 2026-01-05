74075da102e03869ff344b1be7ad34481aa3cd25
Horm Thida <your_email@example.com>
2026-01-05 15:38:37 +0700

    fix 01

--- school_management_app/settings.py
+++ school_management_app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-fzm_c+z$tt@x-!^h-y!lk%470v(vi#m8ww&%piw$ud!c@vsctq
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
